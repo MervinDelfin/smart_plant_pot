4a1ad0c4beee1b93e217f756999ebb7c48243aa7
Marvin Ruciński <marvin@prasa-polska.com>
2023-03-30 18:59:56 +0200

    added button to message

--- api.py
+++ api.py
@@ -3,8 +3,9 @@ from rest_framework import routers, serializers, viewsets
 from .models import State, DefindedState, Message
 from django.utils import timezone
 from datetime import timedelta
-from sienkiewicza114Project.celery import send_bot_ai_message
+from sienkiewicza114Project.celery import send_bot_ai_message_generic_link_button
 from duty.models import Persons
+from django.urls import reverse
 
 router = routers.DefaultRouter()
 class StateSerializer(serializers.ModelSerializer):
@@ -23,11 +24,14 @@ class StateViewSet(viewsets.ModelViewSet):
             not Message.objects.exists() or
             Message.objects.latest('date').date < timezone.now() - timedelta(hours=2)
         ):
-            print("Moisture is too low, please water the plant!")
+            print("Moisture is too low, sending message")
 
             Message.objects.create(message=f"Moisture is too low, please water the plant! {int(request.data['moisture'])/1024*100}%")
 
-            send_bot_ai_message.delay(f"Jesteś botem przypominającym o podlewaniu bazylii. Poziom wilgotności w ziemi własnie spadł poniżej {water_min}% i trzeba podlać roślinę. Napisz śmieszną wiadomość, która przypomni o podlaniu rośliny.", [Persons.objects.get(name="Marvin").chatfuel_id])
+            url = reverse('smart_plant_pot:index')
+            absolute_url = request.build_absolute_uri(url)
+
+            send_bot_ai_message_generic_link_button.delay(f"Jesteś botem przypominającym o podlewaniu bazylii. Poziom wilgotności w ziemi własnie spadł poniżej {water_min}% i trzeba podlać roślinę. Napisz śmieszną wiadomość, która przypomni o podlaniu rośliny.", [Persons.objects.get(name="Marvin").chatfuel_id], absolute_url)
 
         return super().create(request)
 
