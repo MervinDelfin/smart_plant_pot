9182b7f145f4e031c6e8cfce7a71229ecb58a908
Marvin Ruciński <marvin@prasa-polska.com>
2023-03-30 12:08:53 +0200

    add defined states to api

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 
 from rest_framework import routers, serializers, viewsets
-from .models import State
+from .models import State, DefindedState
 
 router = routers.DefaultRouter()
 class StateSerializer(serializers.ModelSerializer):
@@ -13,3 +13,14 @@ class StateViewSet(viewsets.ModelViewSet):
     serializer_class = StateSerializer
 
 router.register(r'states', StateViewSet, basename='states')
+
+class DefinedStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DefindedState
+        fields = '__all__'
+
+class DefinedStateViewSet(viewsets.ModelViewSet):
+    queryset = DefindedState.objects.all()
+    serializer_class = DefinedStateSerializer
+
+router.register(r'defined_states', DefinedStateViewSet, basename='defined_states')
